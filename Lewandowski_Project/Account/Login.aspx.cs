b782783e44ab7cba5591893b7eb244ed7c037084
Ryan Fitch <rfitch19@jcu.edu>
2018-10-17 22:34:36 -0400

    with storage procedure example

--- Lewandowski_Project/Account/Login.aspx.cs
+++ Lewandowski_Project/Account/Login.aspx.cs
@@ -5,70 +5,58 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using Lewandowski_Project.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace Lewandowski_Project.Account
 {
     public partial class Login : Page
     {
+        string dbConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            RegisterHyperLink.NavigateUrl = "Register";
-            // Enable this once you have account confirmation enabled for password reset functionality
-            //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
-            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
-            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
-            if (!String.IsNullOrEmpty(returnUrl))
-            {
-                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
-            }
+            
         }
 
         protected void LogIn(object sender, EventArgs e)
         {
-            if(Email.Text == "student@gmail.com")
-            {
-                this.Master.FindControl("studentPage").Visible = true;
-            }
-            if (Email.Text == "practitioner@gmail.com")
-            {
-                this.Master.FindControl("practitionerPage").Visible = true;
-            }
-            if (Email.Text == "admin@gmail.com")
-            {
-                this.Master.FindControl("adminPage").Visible = true;
-            }
+            this.Master.FindControl("studentPage").Visible = true;
+            this.Master.FindControl("practitionerPage").Visible = true;
+            this.Master.FindControl("adminPage").Visible = true;
 
-            /* if (IsValid)
+            validateLogIn(Email.Text, Password.Text);
+        }
+
+        protected void validateLogIn(string emailInput, string passwordInput)
+        {
+            using (MySqlConnection con = new MySqlConnection(dbConnectionString))
             {
-                // Validate the user password
-                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
-
-                // This doen't count login failures towards account lockout
-                // To enable password failures to trigger lockout, change to shouldLockout: true
-                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
-
-                switch (result)
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand("login_procedure", con))
                 {
-                    case SignInStatus.Success:
-                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                        break;
-                    case SignInStatus.LockedOut:
-                        Response.Redirect("/Account/Lockout");
-                        break;
-                    case SignInStatus.RequiresVerification:
-                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
-                                                        Request.QueryString["ReturnUrl"],
-                                                        RememberMe.Checked),
-                                          true);
-                        break;
-                    case SignInStatus.Failure:
-                    default:
-                        FailureText.Text = "Invalid login attempt";
-                        ErrorMessage.Visible = true;
-                        break;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    //an in parameter
+
+                    cmd.Parameters.AddWithValue("users_UserName", emailInput);
+                    cmd.Parameters["users_UserName"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.AddWithValue("users_Password", passwordInput);
+                    cmd.Parameters["users_Password"].Direction = ParameterDirection.Input;
+
+                    //an out parameter
+
+                    cmd.Parameters.AddWithValue("users_PersonId", MySqlDbType.Int32);
+                    cmd.Parameters["users_PersonId"].Direction = ParameterDirection.Output;
+
+
+                    Email.Text =  cmd.ExecuteNonQuery().ToString();
+
+                   
                 }
-            }*/
+            }
         }
     }
 }
